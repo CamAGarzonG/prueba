f2b47de6030b2475c1ff062a2b95989801bb93fa
camilo_garzon <caagarzonga@gmail.com>
2021-06-21 10:59:35 -0500

    Commit a master, creacion clase (paso 6)

---
+++ src/Data/Calculator.java
@@ -0,0 +1,11 @@
+package Data;
+
+public class Calculator {
+    private float number1, number2, result;
+
+    public Calculator(float number1, float number2, float result) {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.result = result;
+    }
+}
